96d498df0130f251ac6eb9ed45d09b111fc7a32f
Brian Feldman <brian.feldman42@gmail.com>
2020-08-25 14:30:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   string_array = string.split(" ")
-  # new_array = []
   string_array.collect do |word|
     word.reverse!
   end
-  # new_array.join(" ")
+  string_array.join(" ")
 end
